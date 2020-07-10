4b924715d3607700698de38356917d5af8c57868
Vikta5 <vikta5kozlova@gmail.com>
2020-07-10 15:52:17 +0300

    Add sound singletor

---
+++ Assets/Code/Gameplay/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    static SoundManager Instance
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+
+    public static SoundManager instance = null;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
